303187083e39620871dbbce0c5178c6e84056fb0
amalrajac1997 <amalrajac1997@gmail.com>
2025-04-19 11:55:50 +0530

    updated the code

--- features/login.feature
+++ features/login.feature
@@ -1,4 +1,4 @@
 Feature: The Internet Guinea Pig Website
 
   Scenario: As a user, I can log into the application
-    Given I logged into the application as a standard User
+    Given I logged into the application as standard User
--- features/step-definitions/loginSteps.js
+++ features/step-definitions/loginSteps.js
@@ -1,12 +1,10 @@
 const { Given, When, Then } = require('@wdio/cucumber-framework');
 const { expect, $ } = require('@wdio/globals')
-
 const LoginPage = require('../pageobjects/login.page');
-const SecurePage = require('../pageobjects/secure.page');
 
 const {standardUser,Password}=process.env
 
-Given (/I logged into the application as a standard User$/,async()=>{
+Given (/I logged into the application as standard User$/,async()=>{
     await LoginPage.open();
     await LoginPage.login(standardUser,Password)
     await LoginPage.verifyTheVisibilityOfLogoIconInHomePage();
